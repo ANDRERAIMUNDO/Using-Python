8eaa94711f1d7bc3a94e92ea9d72a0a0edf6042e
ANDRERAIMUNDO <9000andre@gmail.com>
2020-07-21 15:41:41 -0300

    Introdução

---
+++ pep8.py
@@ -0,0 +1,38 @@
+"""
+PEP8 - Python Enhancement Proposal
+São proprostas de melherias para a linguagem Python
+Thr zem of Pythom
+import this
+A ideia da PEP8 é que possamos escrever codigos Pythom em forma Pythônica
+(1) - Ultilize Camel Case para nomes de classes
+(2) - Ultilize nome em minusculo, separados por underline para funções ou variaveis
+(3) - ultilize quatro espaços para identação!
+(4) - Linhas em branco
+-Separar funções e defiições de classe com duas linhas em brancos
+- Metodos de uma classe devem ser separadas com uma unica linha em branco
+(5) imports
+-Imports devem ser sempre feitos em linhas separadas
+-Iports devem ser colocados no topo do arquivo
+"""
+
+
+class Calculadora:
+    pass
+
+
+class CalculadoraCientifica:
+    pass
+
+
+def soma():
+    pass
+
+
+def soma_dois():
+    pass
+
+
+numero = 4
+numero_impar = 5
+if 'a' in 'obj':
+    print('obj')
